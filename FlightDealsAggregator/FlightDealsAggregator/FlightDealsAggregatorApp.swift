5e0f3c7bcb7c984d84ed09fb697d6658e98b5ff2
Aleksandr Vorobei <vorobeisasa@gmail.com>
2025-09-29 09:33:20 +0300

    start project

---
+++ FlightDealsAggregator/FlightDealsAggregator/FlightDealsAggregatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightDealsAggregatorApp.swift
+//  FlightDealsAggregator
+//
+//  Created by Александр Воробей on 29.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlightDealsAggregatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
